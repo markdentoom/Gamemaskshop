c317ef6a1225ca81fe528495066c2864e2ce339d
markdentoom <markdentoom@hotmail.com>
2021-11-14 22:44:04 +0100

    create backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const products = require("./data/products")
+
+const app = express()
+const PORT = 8000
+
+app.get("/", (req, res) => {
+  res.send("API is running...")
+})
+
+app.get("/api/products", (req, res) => {
+  res.json(products)
+})
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id)
+  res.json(product)
+})
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`))
