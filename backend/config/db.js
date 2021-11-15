1a452657372413cd94b99c60ab237cf26f17b117
markdentoom <markdentoom@hotmail.com>
2021-11-15 20:22:23 +0100

    create models

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log(`MongoDB connected: ${conn.connection.host}`)
   } catch (error) {
-    console.log(`Error: ${error.message}`)
+    console.log(`DBError: ${error.message}`)
     process.exit(1)
   }
 }
