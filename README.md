965b1c49bccec750159f837227e5545946e6fb43
markdentoom <markdentoom@hotmail.com>
2021-11-25 14:14:01 +0100

    changed example data and made temporary readme

---
+++ README.md
@@ -0,0 +1,9 @@
+Available npm commands:
+`npm run backend`: start backend server using nodemon
+`npm run frontend`: start frontend server
+`npm run dev`: start frontend and backend server
+`npm run data:destroy`: empty database tables
+`npm run data:import`: empty database tables and populate them with test data
+
+Here is a small preview of the work in progress:
+![image](https://user-images.githubusercontent.com/59030690/143447819-252ed722-700d-479d-af5e-690f54900a35.png)
--- backend/data/products.js
+++ backend/data/products.js
@@ -1,7 +1,7 @@
 const products = [
   {
-    name: "Airpods Wireless Bluetooth Headphones",
-    image: "/images/airpods.jpg",
+    name: "Payday 2 Dallas mask",
+    image: "/images/dallas.jpg",
     description:
       "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
     brand: "Apple",
@@ -12,8 +12,8 @@ const products = [
     numReviews: 12,
   },
   {
-    name: "iPhone 11 Pro 256GB Memory",
-    image: "/images/phone.jpg",
+    name: "Payday 2 Chains mask",
+    image: "/images/chains.jpg",
     description:
       "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
     brand: "Apple",
@@ -24,8 +24,8 @@ const products = [
     numReviews: 8,
   },
   {
-    name: "Cannon EOS 80D DSLR Camera",
-    image: "/images/camera.jpg",
+    name: "Payday 2 Hoxton mask",
+    image: "/images/hoxton.jpg",
     description:
       "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
     brand: "Cannon",
@@ -36,8 +36,8 @@ const products = [
     numReviews: 12,
   },
   {
-    name: "Sony Playstation 4 Pro White Version",
-    image: "/images/playstation.jpg",
+    name: "Payday 2 Wolf mask",
+    image: "/images/wolf.jpg",
     description:
       "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
     brand: "Sony",
@@ -48,20 +48,56 @@ const products = [
     numReviews: 12,
   },
   {
-    name: "Logitech G-Series Gaming Mouse",
-    image: "/images/mouse.jpg",
+    name: "LOZMM Majora's Mask",
+    image: "/images/majora.jpg",
     description:
       "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
     brand: "Logitech",
     category: "Electronics",
-    price: 49.99,
-    countInStock: 7,
-    rating: 3.5,
-    numReviews: 10,
+    price: 162.99,
+    countInStock: 6,
+    rating: 4.5,
+    numReviews: 5,
   },
   {
-    name: "Amazon Echo Dot 3rd Generation",
-    image: "/images/alexa.jpg",
+    name: "LOZMM Zora mask",
+    image: "/images/zora.jpg",
+    description:
+      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 149.99,
+    countInStock: 1,
+    rating: 5,
+    numReviews: 8,
+  },
+  {
+    name: "LOZMM Deku mask",
+    image: "/images/deku.jpg",
+    description:
+      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 111.99,
+    countInStock: 17,
+    rating: 3.5,
+    numReviews: 4,
+  },
+  {
+    name: "LOZMM Goron mask",
+    image: "/images/goron.jpg",
+    description:
+      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 210.95,
+    countInStock: 4,
+    rating: 4.5,
+    numReviews: 14,
+  },
+  {
+    name: "ATLA Blue Spirit mask",
+    image: "/images/spirit.jpg",
     description:
       "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
     brand: "Amazon",
